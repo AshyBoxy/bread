269aa6836da1ab8039fcc8b36d2826e9c031a530
AshyBoxy <ashyboxy@ashyboxy.xyz>
2021-12-13 04:30:56 +0000

    screams

--- src/Events/interactionCreate.ts
+++ src/Events/interactionCreate.ts
@@ -1,6 +1,12 @@
 import EventHandler from "../Classes/EventHandler";
+import { randomInt } from "../Utils/randomNumber";
 
 export default new EventHandler("interactionCreate", () => (int): void => {
-    if (int.isButton() || int.isCommand())
-        int.reply("aaaaaaaaaaa");
+    if (int.isButton() || int.isCommand()) {
+        const replies = [
+            "aaaaa", "AHHHHHH", "ARGHHHHHH", "AAAAAAAAAAAA"
+        ];
+        int.reply(replies[randomInt(0, replies.length - 1)]);
+
+    }
 });
