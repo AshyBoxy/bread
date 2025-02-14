fe2208e7470263c9a9e959a9ed58bd08af7ba39c
AshyBoxy <ashyboxy@ashyboxy.xyz>
2025-02-14 04:39:44 +0000

    terrible things to make proper type checking for user dbs

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,16 @@
 import { BreadClient } from "./src/framework/src";
+import IDatabase from "./src/framework/src/Interfaces/Database";
+import IUserData from "./src/Interfaces/UserData";
 
-// eslint-disable-next-line @typescript-eslint/prefer-namespace-keyword
 declare global {
     // eslint-disable-next-line no-var
     var bot: BreadClient;
 }
 
+declare module "./src/framework/src/Classes/Client" {
+    interface BreadUserDBs {
+        userData: IDatabase<IUserData>;
+    }
+}
 
 export { };
